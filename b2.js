388ced93e10160346f4c24287efec96f0b05e0d7
hahuydz <haquanghuy2006abc@gmail.com>
2025-03-21 03:27:09 +0700

    B2

---
+++ b2.js
@@ -0,0 +1,9 @@
+
+const button = document.querySelector('button');
+const text = document.querySelector('.count');
+let count = 0;
+
+button.addEventListener('click', function(){
+    count++;
+    text.innerHTML = `Số lần bấm: ${count}`;
+})
